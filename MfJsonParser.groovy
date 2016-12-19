49a4c53fd37c5bb5d7cafb0de9fe7bbd2502935c
Miguelángel Fernández <miguelangelxfm@gmail.com>
2016-12-19 23:11:49 +0100

    Parses config and policyConfig into objects.

--- MfJsonParser.groovy
+++ MfJsonParser.groovy
@@ -1,22 +1,33 @@
-
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
 @NonCPS
 def prettify(txt){
-    
-    def jsonOutput = new JsonOutput()
-    def unescaped = jsonOutput.unescaped(txt)
-    assert unescaped instanceof JsonOutput.JsonUnescaped
-
-    println(unescaped)
-    println("flag 0")
-
-    def fixed = unescaped.toString().replaceAll(/"\{/, "{").replaceAll(/\}"/, "}")
-    println(fixed)
-    println("flag 2")
-
-    return JsonOutput.prettyPrint(fixed)
+	println("Prettifying...")
+	println("Orig content: ${txt}")
+	
+	def slurper = new JsonSlurper()
+	
+	def obj = slurper.parseText(txt)
+	
+	if(obj.config != null){
+		def config = obj.config
+		assert config instanceof String
+		obj.config = slurper.parseText(config)
+	}
+	
+	if(obj.policyConfig != null){
+		def policyConfig = obj.policyConfig
+		assert policyConfig instanceof String
+		obj.policyConfig = slurper.parseText(policyConfig)
+	}
+	
+	println("Parsed object: ${obj}")
+	
+	def pretty = JsonOutput.prettyPrint(JsonOutput.toJson(obj))
+	println("Prettified content: ${pretty}")
+	
+	return pretty
 }
 
 return this;
